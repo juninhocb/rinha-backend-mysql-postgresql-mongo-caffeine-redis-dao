ee891f818dd9b8abf7a27886a10fa0e117450706
juninhocb <juninhocb@hotmail.com>
2023-09-05 19:55:59 -0300

    Update Person.kt

--- simple-mvc-kotlin/src/main/kotlin/com/example/carlosjr/simplemvckotlin/person/Person.kt
+++ simple-mvc-kotlin/src/main/kotlin/com/example/carlosjr/simplemvckotlin/person/Person.kt
@@ -5,7 +5,7 @@ import org.springframework.data.mongodb.core.mapping.Document
 
 @Document(collection = "person")
 data class Person(
-    val _id: ObjectId? = null, // MongoDB uses ObjectId as the default primary key
+    val _id: ObjectId? = null,
     val nickname: String,
     val name: String,
     val bornAt: String,
